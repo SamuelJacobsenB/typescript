8a73c874a87149799e448923887ef2676f3bae64
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-17 12:36:22 -0300

    Aula03

---
+++ typescript-cfbcursos/aula03/dist/aula03.js
@@ -0,0 +1,13 @@
+"use strict";
+class Curso {
+    canal = null;
+    curso = null;
+    constructor(canal, curso) {
+        this.canal = canal;
+        this.curso = curso;
+    }
+    ;
+}
+;
+let c1 = new Curso('CFB-Cursos', 'Typescript');
+console.log(c1.canal, c1.curso, 'Deu certo');
