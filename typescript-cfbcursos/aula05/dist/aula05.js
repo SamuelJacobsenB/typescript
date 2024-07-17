a4cbe97958a4e31106b2348d6c38cfbf8a310660
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-17 13:07:45 -0300

    Aula05

---
+++ typescript-cfbcursos/aula05/dist/aula05.js
@@ -0,0 +1,9 @@
+"use strict";
+let teste;
+teste = 'Eu sou um teste';
+teste = 2000;
+let cursos = ['Javascript', 'Typescript', 'Node'];
+let valores = [10, 2.5, 2024];
+cursos.push('React');
+valores.push(14);
+console.log(cursos, valores, teste);
---
+++ typescript-cfbcursos/aula05/src/aula05.ts
@@ -0,0 +1,11 @@
+let teste: string | number;
+teste = 'Eu sou um teste';
+teste = 2000;
+
+let cursos: string[] = ['Javascript', 'Typescript', 'Node'];
+let valores: number[] = [10, 2.5, 2024];
+
+cursos.push('React');
+valores.push(14);
+
+console.log(cursos, valores, teste);
