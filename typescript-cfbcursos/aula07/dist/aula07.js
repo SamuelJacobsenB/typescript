fd16e7d9c84a0215ac04bb75d6790da6ed0c045a
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-18 07:27:28 -0300

    Aula07

---
+++ typescript-cfbcursos/aula07/dist/aula07.js
@@ -0,0 +1,4 @@
+"use strict";
+let coisas = ['Olá mundo', 20, true];
+console.log(coisas);
+console.log(coisas[0]);
---
+++ typescript-cfbcursos/aula07/src/aula07.ts
@@ -0,0 +1,5 @@
+let coisas: readonly[string, number, boolean] = ['Olá mundo', 20, true];
+// coisas[2] = false;
+
+console.log(coisas);
+console.log(coisas[0]);
