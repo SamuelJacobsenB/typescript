b51db723d1ff07d761e2cc16e400ba8af0ea6725
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-19 07:20:20 -0300

    Aula 12

---
+++ typescript-cfbcursos/aula12/dist/aula12.js
@@ -0,0 +1,13 @@
+"use strict";
+function logar(user, password) {
+    console.log(`User: ${user}, Senha: ${password}`);
+}
+;
+logar('Samuel', 'senha1234');
+function soma2(n1, n2) {
+    let res = n1 + n2;
+    return res;
+}
+;
+const res = soma2(10, 5);
+console.log(res);
---
+++ typescript-cfbcursos/aula12/src/aula12.ts
@@ -0,0 +1,16 @@
+function logar(user: string, password: string): void {
+    console.log(`User: ${user}, Senha: ${password}`);
+};
+
+logar('Samuel', 'senha1234');
+
+function soma2(n1: number, n2: number): number {
+    let res: number = n1 + n2;
+    return res; 
+};
+
+const n_res: number = soma2(10,5);
+const s_res: string = soma2(20,10).toString();
+
+console.log(n_res);
+console.log(s_res);
