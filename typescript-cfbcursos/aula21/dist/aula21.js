634f41b131356032d589ac28970ca92302b61ba3
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-20 15:17:17 -0300

    Aula 21

--- typescript-cfbcursos/aula21/dist/aula21.js
+++ typescript-cfbcursos/aula21/dist/aula21.js
@@ -2,18 +2,35 @@
 class Conta {
     numero;
     titular;
-    constructor(numero, titular) {
-        this.numero = numero;
+    constructor(titular) {
+        this.numero = this.gerarNumeroConta();
         this.titular = titular;
+    }
+    ;
+    gerarNumeroConta() {
+        return Math.floor(Math.random() * 100000) + 1;
     }
     ;
 }
 ;
 class ContaPF extends Conta {
+    cpf;
+    constructor(cpf, titular) {
+        super(titular);
+        this.cpf = cpf;
+    }
+    ;
 }
 ;
 class ContaPJ extends Conta {
+    cnpj;
+    constructor(cnpj, titular) {
+        super(titular);
+        this.cnpj = cnpj;
+    }
+    ;
 }
 ;
-const c1 = new ContaPF(123456, 'Samuel');
-const c2 = new ContaPJ(654321, 'Maria');
+const c1 = new ContaPF(101918, 'Samuel');
+const c2 = new ContaPJ(909876, 'Maria');
+console.log(c1.numero);
--- typescript-cfbcursos/aula21/src/aula21.ts
+++ typescript-cfbcursos/aula21/src/aula21.ts
@@ -2,19 +2,32 @@ class Conta {
     public numero: number;
     public titular: string;
 
-    constructor(numero: number, titular: string){
-        this.numero = numero;
+    constructor(titular: string){
+        this.numero = this.gerarNumeroConta();
         this.titular = titular;
+    };
+
+    gerarNumeroConta(): number {
+        return Math.floor(Math.random()*100000) + 1;
+    };
+};
+class ContaPF extends Conta {
+    cpf: number;
+    constructor(cpf: number, titular: string){
+        super(titular);
+        this.cpf = cpf;
     };
 };
 
-class ContaPF extends Conta {
-
-};
-
 class ContaPJ extends Conta {
-
+    cnpj: number;
+    constructor(cnpj: number, titular: string){
+        super(titular);
+        this.cnpj = cnpj;
+    };
 };
 
-const c1 = new ContaPF(123456, 'Samuel');
-const c2 = new ContaPJ(654321, 'Maria');
+const c1 = new ContaPF(101918, 'Samuel');
+const c2 = new ContaPJ(909876, 'Maria');
+
+console.log(c1.numero);
