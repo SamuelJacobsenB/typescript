39e87857003f7d35dbbebae0414297fcaf0068fb
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-21 17:29:24 -0300

    Aula23

---
+++ typescript-cfbcursos/aula23/dist/aula23.js
@@ -0,0 +1,105 @@
+"use strict";
+class Conta {
+    numero;
+    titular;
+    saldoConta;
+    constructor(titular) {
+        this.numero = this.gerarNumeroConta();
+        this.titular = titular;
+        this.saldoConta = 0;
+    }
+    ;
+    gerarNumeroConta() {
+        let gerarNumero = (Math.floor(Math.random() * 100000) + 1).toString();
+        while (gerarNumero.length < 6) {
+            gerarNumero = gerarNumero + '0';
+        }
+        ;
+        const numero = Number(gerarNumero);
+        return numero;
+    }
+    ;
+    info() {
+        console.log(`Titular: ${this.titular}`);
+        console.log(`Número: ${this.numero}`);
+    }
+    ;
+    saldo() {
+        return this.saldoConta;
+    }
+    ;
+    deposito(valor) {
+        this.saldoConta += valor;
+    }
+    ;
+    saque(valor) {
+        if (this.saldoConta >= valor) {
+            this.saldoConta -= valor;
+        }
+        else {
+            console.log('Saldo insuficiente');
+        }
+        ;
+    }
+    ;
+}
+;
+class ContaPF extends Conta {
+    cpf;
+    constructor(cpf, titular) {
+        super(titular);
+        this.cpf = cpf;
+    }
+    ;
+    info() {
+        console.log(`Tipo...: PF`);
+        super.info();
+        console.log(`CPF....: ${this.cpf}`);
+        console.log('------------------------');
+    }
+    ;
+    deposito(valor) {
+        if (valor > 1000) {
+            console.log('Valor ultrapassa o limite de 1.000 reais de uma conta PF');
+        }
+        else {
+            super.deposito(valor);
+        }
+        ;
+    }
+    ;
+}
+;
+class ContaPJ extends Conta {
+    cnpj;
+    constructor(cnpj, titular) {
+        super(titular);
+        this.cnpj = cnpj;
+    }
+    ;
+    info() {
+        console.log(`Tipo...: PJ`);
+        super.info();
+        console.log(`CNPJ...: ${this.cnpj}`);
+        console.log('------------------------');
+    }
+    ;
+    deposito(valor) {
+        if (valor > 10000) {
+            console.log('Valor ultrapassa o limite de 10.000 reais de uma conta PJ');
+        }
+        else {
+            super.deposito(valor);
+        }
+        ;
+    }
+    ;
+}
+;
+const c1 = new ContaPF(101918, 'Samuel');
+const c2 = new ContaPJ(909876, 'Empresa cursos');
+c1.info();
+c2.info();
+console.log('-------------------------------------');
+c2.deposito(5250);
+console.log(c2.saldo());
