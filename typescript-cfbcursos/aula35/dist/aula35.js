d0a84385c03eb206daae5700a32c22ddb60d0971
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-23 15:43:58 -0300

    Aula 35

---
+++ typescript-cfbcursos/aula35/dist/aula35.js
@@ -0,0 +1,9 @@
+"use strict";
+const obj = {
+    cor1: 'Verde',
+    cor2: 'Vermelho',
+    cor3: 'Azul',
+    cor4: 'Amarelo',
+    cor5: 'Branco',
+};
+let { cor1, cor2, cor3, cor4, cor5 } = obj;
